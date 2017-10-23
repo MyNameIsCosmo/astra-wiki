30241d6aa89bf84c1c9175cfb7162b89b783f025
Cosmo Borsky <cosmo@dabitindustries.com>
2017-10-22 21:31:04 -0400

    Cameras are detected and color stream is started

--- scripts/OpenNIQtInterface.py
+++ scripts/OpenNIQtInterface.py
@@ -6,32 +6,38 @@ from pyqtgraph.Qt import QtCore, QtGui
 from OpenNIDevice import *
 
 class CvToQImage(QtGui.QImage):
+
     def __init__(self, img, mapping="RGB"):
-        height, width, channel = img.shape
+        #FIXME: map "mapping" values to qimage formats
+        if len(img.shape) > 2:
+            height, width, channel = img.shape
+        else:
+            height, width = img.shape
         bytesPerLine = 3 * width
         self.mapping = mapping
         #self.qImg = QImage(img.data, width, height, bytesPerLine, QImage.Format_RGB888)
-        super(CvToQImage, self).__init__(img, width, height, QtGui.QImage.Format_RGB888)
+        super(CvToQImage, self).__init__(img.tostring(), width, height, QtGui.QImage.Format_RGB888)
 
 class ImageView(QtGui.QLabel):
     def __init__(self, parent=None):
         super(QtGui.QLabel, self).__init__(parent)
         self._image = None
 
-    def updateFrame(self, img, pixmap=False, height=640, width=480):
+    def updateFrame(self, img, height=640, width=480, pixmap=False):
         self._image = img
         if self._image is not None:
             if pixmap:
                 _image_pixmap = QtGui.QPixmap(self._image)
             else:
                 _image_pixmap = QtGui.QPixmap.fromImage(self._image)
-            _image_resized = _image_pixmap.scaled(height,width,Qt.KeepAspectRatio)
+            _image_resized = _image_pixmap.scaled(height,width,QtCore.Qt.KeepAspectRatio)
             self.setPixmap(_image_resized)
 
 class ImageWidget(QtGui.QWidget):
+
     def __init__(self, parent, image=None, height=640, width=480):
         super(QtGui.QWidget, self).__init__(parent)
-        layout = QVBoxLayout()
+        layout = QtGui.QVBoxLayout()
         self.imageLabel = ImageView(self)
         layout.addWidget(self.imageLabel)
         self.setLayout(layout)
@@ -39,31 +45,65 @@ class ImageWidget(QtGui.QWidget):
         if image:
             self.update(image, height, width)
 
-    def update(self, image, height=640, width=480):
-	self.imageLabel.updateFrame(CvToQImage(cv_image),height=height,width=width)
+    def update(self, image):
+	self.imageLabel.updateFrame(CvToQImage(image))
 
 class DeviceViewer(QtGui.QWidget):
-    def __init__(self, parent=None):
+
+    def __init__(self, parent=None, device=None):
         super(QtGui.QWidget, self).__init__(parent)
+        self.parent_ = parent
+        self.uri, self.make, self.model = device
+        self.setObjectName("{} {}".format(self.make, self.model))
+
         self.__widgets()
         self.__layout()
+        self._init_device(self.uri)
 
     def __widgets(self):
-        #self.widget_image_color = ImageWidget(self)
-        self.widget_image_color = QtGui.QLabel("Work in progress")
+        self.widget_image_color = ImageWidget(self)
+        #self.widget_image_color.resize(640/2,480/2)
+        #self.widget_image_depth = ImageWidget(self)
+        #self.widget_image_depth.resize(640/2,480/2)
 
     def __layout(self):
         self.vbox = QtGui.QVBoxLayout()
         self.hbox = QtGui.QHBoxLayout()
 
         self.hbox.addWidget(self.widget_image_color)
+        #self.hbox.addWidget(self.widget_image_depth)
+        self.vbox.addLayout(self.hbox)
 
+        self.setLayout(self.vbox)
+    
+    def _update_images(self):
+        image_color = self.device.get_frame_color()
+        #image_color = cv2.resize(image_color, (0,0), fx=0.25, fy=0.25) 
+        #image_depth = self.device.get_frame_depth()
+        #image_depth = cv2.resize(image_depth, (0,0), fx=0.25, fy=0.25) 
+
+        #image_depth = cv2.convertScaleAbs(image_depth, alpha=(255.0/65535.0))
+
+        self.widget_image_color.update(image_color)
+        #self.widget_image_depth.update(image_depth)
+
+    def _init_device(self, uri):
+        self.device = OpenNIDevice(uri)
+        #self.device.open_stream_depth()
+        self.device.open_stream_color()
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.start(30) # FIXME: use 1000/FPS or whatever
+        self.timer.timeout.connect(self._update_images)
+        
 class DeviceSelection(QtGui.QWidget):
+
     def __init__(self, parent=None):
         super(QtGui.QWidget, self).__init__(parent)
+        self.setObjectName("Device Selection")
+        self.parent_ = parent
         self.__widgets()
         self.__layout()
-        self._get_devices()
         self._list_devices()
 
     def __widgets(self):
@@ -76,17 +116,22 @@ class DeviceSelection(QtGui.QWidget):
         self.hbox = QtGui.QHBoxLayout()
         self.hbox2 = QtGui.QHBoxLayout()
 
+        self.button_refresh = QtGui.QPushButton("Refresh")
+        self.button_refresh.clicked.connect(self._list_devices)
+        self.button_refresh.setFixedWidth(120)
+
         self.hbox.addWidget(self.deviceListLabel)
+        self.hbox.addItem(QtGui.QSpacerItem(1,1,QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum))
+        self.hbox.addWidget(self.button_refresh)
         self.hbox2.addWidget(self.deviceList)
 
         self.vbox.addLayout(self.hbox)
         self.vbox.addLayout(self.hbox2)
         self.setLayout(self.vbox)
 
-    def _get_devices(self):
-        self.devices = openni_list()
-
     def _list_devices(self):
+        self.deviceList.clear()
+        self.devices = openni_list()
         if len(self.devices) > 0:
             for d in self.devices:
                 item = QtGui.QListWidgetItem("{} {}: {}".format(d[1], d[2], str(d[0])))
@@ -96,14 +141,23 @@ class DeviceSelection(QtGui.QWidget):
             self.deviceList.addItem(item)
 
     def _device_list_clicked(self, index):
-        print self.devices[index.row()]
+        device = self.devices[index.row()]
+        tabToolTips = [unicode(self.parent_.tabWidget.tabToolTip(t)) for t in range(self.parent_.tabWidget.count())]
+        uri = unicode(device[0])
+        if uri in tabToolTips:
+            print "{} already open!".format(device[0])
+            self.parent_.statusBar().showMessage("{} already open in tab #{}".format(uri, tabToolTips.index(uri))) 
+        else:
+            device_view = DeviceViewer(self.parent_, device) 
+            self.parent_.add_tab(device_view, device_view.uri, True)
+            self.parent_.statusBar().showMessage("{} {} opened".format(device[1], device[2])) 
 
 
 class MainWindow(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
         super(QtGui.QMainWindow, self).__init__(parent)
-        self._setWindowSize()
+        self._set_window_size()
         self.__layout()
         self.__init_menu()
         self.statusBar().showMessage('Listing Devices')
@@ -112,18 +166,23 @@ class MainWindow(QtGui.QMainWindow):
 
     def __layout(self):
         _widget = QtGui.QWidget()
-        self.mainWidget = QtGui.QWidget()
         self.layout = QtGui.QVBoxLayout(_widget)
-        self.layout.addWidget(self.mainWidget)
+
+        self.tabWidget = QtGui.QTabWidget()
+        self.tabWidget.setTabPosition(1)
+        self.tabWidget.setTabsClosable(True)
+        self.tabWidget.tabCloseRequested.connect(self._closeTab)
+
+        self.layout.addWidget(self.tabWidget)
         self.setCentralWidget(_widget)
 
     def __init_menu(self):
         self.mainMenu = self.menuBar()
         self.mainMenu.setNativeMenuBar(False)
         self.fileMenu = self.mainMenu.addMenu('&File')
-        self.dataMenu = self.mainMenu.addMenu('Data')
+        #self.dataMenu = self.mainMenu.addMenu('Data')
         self.aboutMenu = self.mainMenu.addMenu('About')
-        self.debugMenu = self.mainMenu.addMenu('Debug')
+        #self.debugMenu = self.mainMenu.addMenu('Debug')
 
         exitButton = QtGui.QAction(QtGui.QIcon(), 'Exit', self)
         exitButton.setShortcut("Ctrl+Q")
@@ -131,25 +190,43 @@ class MainWindow(QtGui.QMainWindow):
         exitButton.triggered.connect(self.close)
         self.fileMenu.addAction(exitButton)
 
-    def _setWindowSize(self, width=800, height=600):
+    def _destruct(self):
+        print "Destruction TODO"
+
+    def _set_window_size(self, width=800, height=600):
         self.resize(width, height)
         self.setMinimumSize(width, height)
         self.setMaximumSize(width, height)
 
-    def _set_main_widget(self, cls):
-        self.mainWidget.close()
-        self.layout.removeWidget(self.mainWidget)
-        self.mainWidget = cls(self)
-        self.layout.addWidget(self.mainWidget)
+    def _closeTab (self, currentIndex):
+        #FIXME: Handle openni close
+        currentQWidget = self.tabWidget.widget(currentIndex)
+        currentQWidget.deleteLater()
+        self.tabWidget.removeTab(currentIndex)   
+
+    def add_tab(self, cls, tooltip=None, closable=False, icon=None):
+        try:
+            name = cls.objectName()
+        except Exception, e:
+            print e
+            name = "Unknown"
+        tab = self.tabWidget.addTab(cls, name)
+        if tooltip:
+            self.tabWidget.setTabToolTip(tab, tooltip)
+        if not closable:
+            self.tabWidget.tabBar().setTabButton(tab, QtGui.QTabBar.RightSide,None)
+        if icon:
+            self.tabWidget.setTabIcon(icon)
+	self.tabWidget.setCurrentIndex(tab)
 
     def show_device_list(self):
-        self._set_main_widget(DeviceSelection)
+        device_list = DeviceSelection(self)
+        self.add_tab(device_list)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
 
     main = MainWindow()
     main.show()
-    #app.aboutToQuit.connect(main_window.destruct)
+    app.aboutToQuit.connect(main._destruct)
     app.exec_()
-        #main_window.destruct()
