056269751582b801724efe9f2497f92d25193ea4
Cosmo Borsky <cosmo@dabitindustries.com>
2017-10-29 17:20:22 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DepthInterface',
+      version='0.1',
+      description='OpenNI Depth Camera Interface',
+      author='Cosmo Borsky',
+      author_email='me@cosmoborsky.com',
+      packages=['DepthInterface'],
+      package_dir={'': 'src'}
+     )
